3d137515d3e6a442893e36f4b4ee6dcbd39d07ca
JesterIruka <jesteriruka@gmail.com>
2020-09-13 00:20:06 -0300

    1.0.2

---
+++ index.d.ts
@@ -0,0 +1,23 @@
+export interface AskSocket {
+  on(event:'open'|'close', callback: Function);
+  emitNet(event:string, ...args: any[]);
+  ask(question: string): Promise<any>;
+  close(): void;
+}
+
+export interface AskSocketServer {
+  on(event:'open', callback: SocketCallback);
+  on(event:'close', callback: SocketCallback);
+  on(event:'listening', callback: Function);
+  close(callback: ErrorCallback);
+}
+
+export interface ErrorCallback {
+  (err: Error): void;
+}
+
+export interface SocketCallback {
+  (as: AskSocket): any;
+}
+
+export interface TimeoutError extends Error {};
--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const AskSocket = require('./src/AskSocket');
 const AskSocketServer = require('./src/AskSocketServer');
 const TimeoutError = require('./src/TimeoutError');
 
-module.exports = { AskSocket, AskSocketServer, TimeoutError };
+module.exports.AskSocket = AskSocket;
+module.exports.AskSocketServer = AskSocketServer;
+module.exports.TimeoutError = TimeoutError;
